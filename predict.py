36a8872d2aa67045f247efa3603f183e227339b0
William Huang <williamhxy@outlook.com>
2020-05-11 12:40:48 -0700

    added a class for prediction

---
+++ predict.py
@@ -0,0 +1,70 @@
+import csv
+import numpy as np
+import random
+import tokenizer as tk
+from keras.models import load_model
+
+class Predict:
+    def __init__(self, k, dict_file_path, model_path):
+        '''
+        initialize the RNN model with the input dimension and length
+        Args:
+            k: the kmer value
+            dict_file_path: file path to the dictionary
+            model_path: the path to the model
+        '''
+        self.kmer = k
+        self.model = load_model(model_path)
+
+        mapping = list()
+        # import the .csv as a list
+        with open(dict_file_path, 'r') as readfile:
+            seq_reader = csv.reader(readfile)
+            for row in seq_reader:
+                mapping.append(row[0])
+        self.dict = mapping
+
+    def predict(self, primary_seq):
+        secondary_seq = []
+        tokenized = []
+        output = ''
+
+        # tokenize the input sequence based on the input k value and dictionary
+        for i in range(1, len(primary_seq), self.kmer):
+            piece = primary_seq[i:i+self.kmer]
+            try:
+                tokenized.append(self.dict.index(piece))
+            except:
+                print(piece, 'not found ')
+                tokenized.append(0)
+
+        # format the features
+        tokenized = np.asarray(tokenized)
+        tokenized = np.reshape(tokenized, (len(tokenized), 1))
+
+        # predict
+        default = 'N'
+        for k in range(1, self.kmer, 1):
+            default = default + 'N'
+
+        for t in range(len(tokenized)):
+            if tokenized[t] != 0:
+                prediction = (np.argmax(self.model.predict(tokenized[t])))
+                secondary_seq.append(self.dict[prediction])
+            else:
+                secondary_seq.append(default)
+
+        # stitch the list into a single string
+        output = output.join(secondary_seq)
+
+        return output
+
+
+if __name__ == "__main__":
+    lines = tk.purge('RNA_sequence_input.csv')
+    random.shuffle(lines)
+
+    predictor = Predict(6, '6_CNN.csv', '6_kmer_RNN.h5')
+    predicted = predictor.predict(lines[1][0])
+    print(predicted)
+    print(lines[1][1])
